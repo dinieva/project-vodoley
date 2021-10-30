fd2943eec1c7b610718c66148fd04a21c2bc1f9a
Rezeda <dinievarezeda@mail.ru>
2021-10-30 03:47:00 +0300

    adaptive

--- js/script.js
+++ js/script.js
@@ -89,3 +89,34 @@ dots.forEach(function(item, indexDot) {
 })
 next.addEventListener('click', nextSlide);
 prev.addEventListener('click', prevSlide);
+
+//бургер меню
+const menu = document.querySelector('.header .menu'),
+    menuItem = document.querySelectorAll('.menu_item'),
+    hamburger = document.querySelector('.hamburger');
+
+    hamburger.addEventListener('click', () => {
+        hamburger.classList.toggle('hamburger_active');
+        menu.classList.toggle('menu_active');
+    });
+
+    menuItem.forEach(item => {
+        item.addEventListener('click', () => {
+            hamburger.classList.toggle('hamburger_active');
+            menu.classList.toggle('menu_active');
+        })
+    })
+// function showMenu() {
+//     console.log("hey");
+//     hamburger.classList.toggle('hamburger_active');
+//     menu.classList.toggle('_active');
+// }
+// hamburger.addEventListener('click', showMenu);
+
+// menuItem.forEach(item => {
+//     item.addEventListener('click', () => {
+//         hamburger.classList.toggle('hamburger_active');
+//         menu.classList.toggle('menu_active');
+//     })
+// })
+
